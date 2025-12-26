fe7c03078f3bb97bbe96b764a2c95f92f17d9fec
Hounaida202 <etrichih@gmail.com>
2025-12-26 10:49:13 +0100

    feat : l'ajout de repository de la zone

---
+++ demo1/src/main/java/com/example/demo/repository/ZoneRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+
+
+}
