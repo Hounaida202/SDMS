4c807b31ace3216189f87a5088a5e0112275ca54
Hounaida202 <etrichih@gmail.com>
2025-12-26 11:15:06 +0100

    feat: création de la classe de la gestion centralisé des exceptions des ressources non trouvés

---
+++ demo1/src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package com.example.demo.exception;
+
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String resource, String field, Object value) {
+        super(String.format("%s non trouvé avec %s : '%s'", resource, field, value));
+    }
+}
+
