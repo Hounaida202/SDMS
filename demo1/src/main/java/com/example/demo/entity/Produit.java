8ed475c7835ecb1e2e6f88f9fbc132cce0f146ad
Hounaida202 <etrichih@gmail.com>
2025-12-26 10:10:00 +0100

    feat : creation de l entity produit

---
+++ demo1/src/main/java/com/example/demo/entity/Produit.java
@@ -0,0 +1,51 @@
+package com.example.demo.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produit")
+public class Produit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(max = 100)
+    @NotNull
+    @Column(name = "nom", nullable = false, length = 100)
+    private String nom;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "categorie", nullable = false, length = 50)
+    private String categorie;
+
+    @NotNull
+    @Column(name = "poids", nullable = false, precision = 10, scale = 2)
+    private BigDecimal poids;
+
+    @NotNull
+    @Column(name = "prix", nullable = false, precision = 10, scale = 2)
+    private BigDecimal prix;
+
+    // Getters et Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+
+    public String getCategorie() { return categorie; }
+    public void setCategorie(String categorie) { this.categorie = categorie; }
+
+    public BigDecimal getPoids() { return poids; }
+    public void setPoids(BigDecimal poids) { this.poids = poids; }
+
+    public BigDecimal getPrix() { return prix; }
+    public void setPrix(BigDecimal prix) { this.prix = prix; }
+}
