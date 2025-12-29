4dfc8aaf63b5e83171cbe3c0749243396b559c64
Hounaida202 <etrichih@gmail.com>
2025-12-29 10:58:06 +0100

    feat: la creation de la classe controller du client expediteur

---
+++ demo1/src/main/java/com/example/demo/controller/ClientExpediteurController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.ClientExpediteurDTO;
+import com.example.demo.service.ClientExpediteurService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/clients")
+public class ClientExpediteurController {
+
+
+}
