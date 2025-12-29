357e3c81e09e21a3b70b092b8ead25076fa13b32
Hounaida202 <etrichih@gmail.com>
2025-12-29 11:51:56 +0100

    feat: la creation des endmpoits , la creation de destination et le recuperer par son id

--- demo1/src/main/java/com/example/demo/controller/DestinataireController.java
+++ demo1/src/main/java/com/example/demo/controller/DestinataireController.java
@@ -1,4 +1,30 @@
 package com.example.demo.controller;
 
+
+import com.example.demo.dto.DestinataireDTO;
+import com.example.demo.service.DestinataireService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/destinataires")
 public class DestinataireController {
+
+    @Autowired
+    private DestinataireService service;
+
+    @PostMapping
+    public ResponseEntity<DestinataireDTO> creer(@Valid @RequestBody DestinataireDTO dto) {
+        DestinataireDTO created = service.creer(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DestinataireDTO> getById(@PathVariable Long id) {
+        DestinataireDTO destinataire = service.getById(id);
+        return ResponseEntity.ok(destinataire);
+    }
 }
