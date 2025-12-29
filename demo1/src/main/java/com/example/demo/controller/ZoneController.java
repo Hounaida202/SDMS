e21dfe2256ba123834a494295ce5d5616b41c5db
Hounaida202 <etrichih@gmail.com>
2025-12-29 11:38:33 +0100

    feat: la creation d une classe controller vide , pour les endpoint de la zone service

---
+++ demo1/src/main/java/com/example/demo/controller/ZoneController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.ZoneDTO;
+import com.example.demo.service.ZoneService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/zones")
+public class ZoneController {
+
+
+}
