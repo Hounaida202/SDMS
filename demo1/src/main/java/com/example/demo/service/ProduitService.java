6ac264e5e376d8d743c7ca94bdb91fc9b4061df6
Hounaida202 <etrichih@gmail.com>
2025-12-29 21:59:55 +0100

    feat : la creation de la classe service transactional des produits

---
+++ demo1/src/main/java/com/example/demo/service/ProduitService.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.ProduitDTO;
+import com.example.demo.entity.Produit;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.mapper.ProduitMapper;
+import com.example.demo.repository.ProduitRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ProduitService {
+
+
+
+}
