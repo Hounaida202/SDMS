a312540729a835d92884ffdfd7adfeeb83710cbf
Hounaida202 <etrichih@gmail.com>
2025-12-29 10:18:04 +0100

    feat: la creation de la classe service pour la logique metier du client expediteur

--- demo1/src/main/java/com/example/demo/service/ClientExpediteurService.java
+++ demo1/src/main/java/com/example/demo/service/ClientExpediteurService.java
@@ -1,4 +1,26 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.ClientExpediteurDTO;
+import com.example.demo.entity.ClientExpediteur;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.mapper.ClientExpediteurMapper;
+import com.example.demo.repository.ClientExpediteurRepository;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@Data
+@NoArgsConstructor
+@Slf4j
 public class ClientExpediteurService {
+
+
+
+
 }
