5846b9b524aa276e484849ac7352cd44229e254d
Hounaida202 <etrichih@gmail.com>
2025-12-29 23:22:35 +0100

    feat : la creation des endpoints de creation et reuperation des produits

--- demo1/src/main/java/com/example/demo/controller/ProduitController.java
+++ demo1/src/main/java/com/example/demo/controller/ProduitController.java
@@ -1,10 +1,30 @@
 package com.example.demo.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.ProduitDTO;
+import com.example.demo.service.ProduitService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/produits")
 public class ProduitController {
+
+    @Autowired
+    private ProduitService service;
+
+    @PostMapping
+    public ResponseEntity<ProduitDTO> creer(@Valid @RequestBody ProduitDTO dto) {
+        ProduitDTO created = service.creer(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProduitDTO> getById(@PathVariable Long id) {
+        ProduitDTO produit = service.getById(id);
+        return ResponseEntity.ok(produit);
+    }
 }
