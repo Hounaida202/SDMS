13c951166d53d701e657107f29191d8ecf74ebfe
Hounaida202 <etrichih@gmail.com>
2026-01-05 12:27:20 +0100

    feat : creation de la classe entity Role

--- demo1/src/main/java/com/example/demo/entity/Role.java
+++ demo1/src/main/java/com/example/demo/entity/Role.java
@@ -1,4 +1,51 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
+@Entity
+@Table(name = "roles")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Le nom du rôle est obligatoire")
+    @Size(max = 50)
+    @Column(unique = true, nullable = false, length = 50)
+    private String name;
+
+
+    public Role() {}
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
