23ed40cce3f15f286c1ba85a585ec84538ffc030
Hounaida202 <etrichih@gmail.com>
2026-01-05 12:27:56 +0100

    feat : creation de la classe entity User

--- demo1/src/main/java/com/example/demo/entity/User.java
+++ demo1/src/main/java/com/example/demo/entity/User.java
@@ -1,4 +1,43 @@
 package com.example.demo.entity;
 
+
+//import com.example.demo.enumeration.AuthProvider;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 150)
+    private String email;
+
+    @Column(nullable = false, length = 50)
+    private String username;
+
+    @Column(nullable = true)
+    private String password;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private Boolean enabled = true;
+
+
 }
